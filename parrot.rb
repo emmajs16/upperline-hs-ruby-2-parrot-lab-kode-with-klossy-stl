a197ea0d92ef63ea10eeb3aac8fe948c590af606
Emma Stoverink <emmajs16@live.com>
2016-06-21 19:29:19 +0000

    We finished the parrot labgit add parrot.rb!

--- parrot.rb
+++ parrot.rb
@@ -4,32 +4,42 @@ def happy_parrot(thing)
 end
 
 # Boring Parrot - Write a method for a boring parrot that just returns whatever string you give him as an argument.
+def boring_parrot(phrase)
+  "#{phrase}"
+end
 
 
-
-# Math Parrot - Create a method that accepts two numbers as arguments and adds them together! 
-
+# Math Parrot - Create a method that accepts two numbers as arguments and adds them together!
+def math_parrot(x,y)
+  x+y
+end
 
 
 # Friendly Parrot - This parrot greets people by returning their name and age (don't forget to pass that information in as arguments).
-
-
+def friendly_parrot(name,age)
+  "Hello,#{name}! You are #{age} years old!"
+end
 
 # Favorites Parrot - Tell this parrot about your three favorite things and he will return "I love <that thing> too!" for each of them.
 
+def favorites(thing)
+  "I love #{thing} too!"
+end
 
-
+def age(age)
+  "You are #{age} old? You're ancient "
+end
 # Now try calling your methods below with any arguments of your choice and puts them to the screen. Like this:
 puts happy_parrot("waffles")
 # call your methods here
-
-
-# Now let's pretend you are a wizard and you want to place a spell on each of the parrots so that they speak backwards. How would you do that?
+puts boring_parrot("Hello!")
+puts math_parrot(4,7)
+puts friendly_parrot("Molly", "15")
+puts favorites("cake")
+puts age(78)
+# Now let's pretend you are a wizard and you want to place a spell on each of the parrot's so that they speak backwards. How would you do that?
 
 
 
 
 # The spell has been broken and everyone is speaking normally again. The parrots are really excited about it though - make them shout in all caps.
-
-
-
